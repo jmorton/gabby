e38d708b451c2c37a7264d72add4ce9bb0bab86a
Jon Morton <jmorton@customink.com>
2013-02-07 10:43:51 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject gabby "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [jivesoftware/smack "3.1.0"]
+                 [com.novemberain/langohr "1.0.0-beta9"]])
