aa00ec16bafe6d706f16757ba6755571c54501ac
Jon Morton <jmorton@customink.com>
2013-02-07 16:23:10 -0500

    Clean up the AMQP-XMPP bridge.

--- src/gabby/rabbit.clj
+++ src/gabby/rabbit.clj
@@ -1,5 +1,5 @@
 (ns gabby.rabbit
-  ^{:author "Jon Morton"}
+  "Bridge between XMPP and AMQP.  Intended for observation (read) only."
   (:gen-class)
   (:use [gabby.core :only (say) :as g])
   (:require [langohr.core       :as rmq]
@@ -9,46 +9,43 @@
             [langohr.consumers  :as lc]
             [langohr.basic      :as lb]))
 
+; I'm still learning how to structure programs in Clojure...
 (def xmpp-credentials (read-string (slurp "config.clj")))
 (def amqp-credentials (merge rmq/*default-config* (read-string (slurp "amqp.clj"))))
 
-; These are shared between everyone that wants to get notified.
-; I'm not sure if this is proper or not...
-(def conn (atom (rmq/connect amqp-credentials)))
-(def chan (atom (lch/open @conn)))
+; Is it ok to reference these diretly in functions?
+(def connection (atom (rmq/connect)))
+(def channel (atom (lch/open @connection)))
 (def bot (g/authenticate xmpp-credentials))
 
-(defn start-consumer
-  [channel queue exchange-name routing-key callback]
-  (let [queue' (.getQueue (lq/declare channel "" :exclusive true :auto-delete true))]
-    (lq/bind channel queue' exchange-name :routing-key "#")
-    (.start (Thread. (fn [] (lc/subscribe channel queue' callback :auto-ack true))))))
+(defn create-queue
+  "Creates a queue and binds it to an exchange.  When the queue receives a message,
+   it invokes f.  You can use create-relay to build a suitable function."
+  [exchange route-key f]
+  (let [queue (lq/declare @channel "" :exclusive true :auto-delete true)
+        queue-name (.getQueue queue)]
+    (lq/bind @channel queue-name exchange :routing-key route-key)
+    (future (lc/subscribe @channel queue-name f :auto-ack true))))
 
-; Builds a function that causes queue messages to send a Jabber message
-; to the person associated with the chat.
-(defn handler [chat message]
-  (fn [ch {:keys [routing-keys] :as meta} ^bytes payload]
+(defn create-generic-relay [chat]
+  "Returns a function for turning AMQP messages into an XMPP message."
+  (fn [ch metadata ^bytes payload]
     (g/say chat (String. payload "UTF-8"))))
 
-(defn subscribe [chat message]
+(defn handle-subscription [chat message]
+  "Process a message from an XMPP user.  If it 'understands' what someone
+   wants, then it will create a queue with a generic relay to notify them
+   when AMQP messages are being received."
+  ; ...that pattern is good enough for now...
   (let [pattern #"subscribe: ([0-9a-z\.\-\_]+)/([0-9a-z\.\-\_\#\*]+)"
         [match exchange routing-key] (re-matches pattern (:body message))]
-    (cond
-     (nil? match) (g/say chat "Sorry, I don't understand that, I'm new here.")
-     :else (do
-             (start-consumer @chan "" exchange routing-key (handler chat message))
-             (g/say chat (str "I subscribed you to '" exchange "' with the key '" routing-key "'"))))))
-
-; not sure if this is the right way to set values or not...
-(defn live [])
-
-; ...or the best way to cleanup.
-(defn die []
-  (g/logout bot)
-  (rmq/close @chan)
-  (rmq/close @conn))
+    (if match
+      (do
+        (create-queue exchange routing-key (create-generic-relay chat))
+        (g/say chat (str "ok"))
+        true))))
 
 (defn- main [& args]
 (let []
-  (g/chat-started bot subscribe)
-  (loop [] (Thread/sleep 1000) (recur))))
+  (g/chat-started bot handle-subscription)
+  (loop [] (Thread/sleep 10000) (recur))))
