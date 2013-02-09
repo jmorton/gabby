1c361ba8ae799020d24b2ce2a669372f64e71a22
Jon Morton <jmorton@customink.com>
2013-02-08 20:16:04 -0500

    Isolate connection expressions.
    - When requiring gabby.rabbit, don't automatically connect to XMPP and AMQP server.

--- src/gabby/rabbit.clj
+++ src/gabby/rabbit.clj
@@ -10,33 +10,33 @@
             [langohr.basic      :as lb]))
 
 ; I'm still learning how to structure programs in Clojure...
-(def xmpp-credentials (read-string (slurp "config.clj")))
-(def amqp-credentials (merge rmq/*default-config* (read-string (slurp "amqp.clj"))))
-
-; Is it ok to reference these diretly in functions?
-(def connection (atom (rmq/connect)))
-(def channel (atom (lch/open @connection)))
-(def bot (g/authenticate xmpp-credentials))
+; I don't want to connect to AMQP or XMPP servers quite yet.
+(declare connection channel bot)
 
 (defn create-queue
   "Creates a queue and binds it to an exchange.  When the queue receives a message,
    it invokes f.  You can use create-relay to build a suitable function."
   [exchange route-key f]
-  (let [queue (lq/declare @channel "" :exclusive true :auto-delete true)
+  (println "Creating a queue")
+  (let [queue (lq/declare channel "" :exclusive true :auto-delete true)
         queue-name (.getQueue queue)]
-    (lq/bind @channel queue-name exchange :routing-key route-key)
-    (future (lc/subscribe @channel queue-name f :auto-ack true))))
+    (future ; non-blocking... make sense?
+      (lq/bind channel queue-name exchange :routing-key route-key)
+      (lc/subscribe channel queue-name f :auto-ack true))))
+
 
 (defn create-generic-relay [chat]
   "Returns a function for turning AMQP messages into an XMPP message."
+  (println "Creating a generic relay")
   (fn [ch metadata ^bytes payload]
     (g/say chat (String. payload "UTF-8"))))
+
 
 (defn handle-subscription [chat message]
   "Process a message from an XMPP user.  If it 'understands' what someone
    wants, then it will create a queue with a generic relay to notify them
    when AMQP messages are being received."
-  ; ...that pattern is good enough for now...
+  (println "Handling a subscription (maybe)")
   (let [pattern #"subscribe: ([0-9a-z\.\-\_]+)/([0-9a-z\.\-\_\#\*]+)"
         [match exchange routing-key] (re-matches pattern (:body message))]
     (if match
@@ -45,7 +45,15 @@
         (g/say chat (str "ok"))
         true))))
 
-(defn- main [& args]
+
+(defn start [& args]
 (let []
-  (g/chat-started bot handle-subscription)
+    (def connection
+      (rmq/connect (merge rmq/*default-config* (read-string (slurp "amqp.clj")))))
+    (def channel
+      (lch/open connection))
+    (def bot
+      (g/authenticate (read-string (slurp "config.clj"))))
+
+    (g/chat-started bot handle-subscription)
   (loop [] (Thread/sleep 10000) (recur))))
