bb0b7a2b2638026bda695072d49e54c07bc3cd7f
Jon Morton <jmorton@customink.com>
2013-02-28 10:36:47 -0500

    Add more utility test coverage

--- test/gabby/util_test.clj
+++ test/gabby/util_test.clj
@@ -22,7 +22,7 @@
            [["*" "*" "jack"]
             ["*" "jack" "be"]
             ["jack" "be" "nimble"]
-            ["be" "nimble" "Jack"]
+            ["be" "nimble" "jack"]
             ["nimble" "jack" "be"]
             ["jack" "be" "quick"]
             ["be" "quick" "~"]])))
@@ -36,4 +36,24 @@
                       hands of some amazing conductor playing all the symphonies
                       of blazing and burning to bring down the tatters and
                       charcoal ruins of history."))))
-    (is (= 1 (count (sentences "P.T. Barnum owns a circus."))))))
+    (is (= 3 (count (sentences "P.T. Barnum owns a circus.")))))
+  (testing "Word count"
+    (is (= 1 (word-count "One")))
+    (is (= 2 (word-count "Hello World!")))
+    ; Not quite right, but if it changes, that could throw
+    ; other things off.
+    (is (= 6 (word-count "P.T. Barnum owns a circus."))))
+  (testing "Perplexity"
+    (is
+     (let [params {["*" "*" "the"]        1
+                   ["*" "the" "dog"]      0.5
+                   ["*" "the" "cat"]      0.5
+                   ["the" "cat" "walks"]  1
+                   ["cat" "walks" "~"]    1
+                   ["the" "dog" "runs"]   1
+                   ["dog" "runs" "~"]     1
+                   }
+           test ["the dog runs"
+                 "the cat walks"
+                 "the dog runs"]]
+       (is (> 1.19 (perplexity test params) 1.189))))))
