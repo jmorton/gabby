3ef1f03b66f1f27f2eaebe729e45625aa7ae1e6f
Jon Morton <jmorton@customink.com>
2014-01-08 11:02:37 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject gabby "0.1.0-SNAPSHOT"
   :description "Gabby (loves to chat) is an XMPP library for Clojure that stands on the shoulders of Smack."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/jmorton/gabby"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [jivesoftware/smack "3.1.0"]
-                 [com.novemberain/langohr "1.0.0-beta11"]]
+                 [com.novemberain/langohr "2.0.1"]]
   :plugins [[lein-marginalia "0.7.1"]])
